f5b330d49fc01b351f703e6f2a614db348cf2b3e
muhammadsaif9162 <127455542+muhammadsaif9162@users.noreply.github.com>
2023-08-19 05:16:55 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Hero from "./sections/Hero"
 import Popular from "./sections/Popular"
 import Superb from "./sections/Superb"
